93aab434ee73badd6db0a98e41078b5c52e90810
Hamza El Meknassi <elmeknassi.h@gmail.com>
2018-12-06 17:53:04 +0100

    Added TimelineComponent and ArticleService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { ArticleService } from './article.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ArticleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
