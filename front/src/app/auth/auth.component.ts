72e293ccfba167a449f14938d5bc580028489c12
Hamza El Meknassi <elmeknassi.h@gmail.com>
2018-12-13 15:40:47 +0100

    Basic authentification working; edited TSlint rules

--- front/src/app/auth/auth.component.ts
+++ front/src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -9,12 +11,32 @@ export class AuthComponent implements OnInit {
   @ViewChild('usrinput') private usrinput: ElementRef;
   @ViewChild('pwdinput') private pwdinput: ElementRef;
 
-  constructor() { }
+  private authError: any;
+
+  constructor(
+    private _authService: AuthenticationService,
+    private _router: Router
+  ) {
+    this.authError = {
+      visible: false,
+      message: ''
+    };
+   }
 
   ngOnInit() {
   }
 
   authenticate() {
     console.log(this.usrinput.nativeElement.value, this.pwdinput.nativeElement.value);
+    if (this._authService.login(this.usrinput.nativeElement.value, this.pwdinput.nativeElement.value))
+      this._router.navigate(['']);
+    else {
+      this.authError.message = 'Cet identifiant et ce mot de passe ne correspondent à aucun utilisateur.';
+      this.authError.visible = true;
+    }
+  }
+
+  handleCloseNotification() {
+    this.authError.visible = false;
   }
 }
---
+++ front/src/app/authentication.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private dbMockup: Array<User>;
+  private currentUser: User;
+
+  constructor() {
+    this.currentUser = undefined;
+    this.dbMockup = [];
+    this.dbMockup.push(new User('pop', 'lol', 'pop@gmail.com'));
+    this.dbMockup.push(new User('dud', 'lol', 'dud@gmail.com'));
+    this.dbMockup.push(new User('top', 'lol', 'top@gmail.com'));
+  }
+
+  login(usr: string, pwd: string): User {
+    const u = this.dbMockup.find((user) => (user.getUsername() === usr && user.getPassword() === pwd));
+    this.currentUser = u;
+    console.log('found user ', u);
+    if (u)
+      return { ...u } as User;
+    else
+      return undefined;
+  }
+
+  disconnect() {
+    this.currentUser = undefined;
+  }
+
+  signup(usr: string, pwd: string, eml: string): User {
+    if (this.dbMockup.find((user) => (user.getUsername() === usr || user.getEmail() === eml)))
+      return undefined;
+    else {
+      this.dbMockup.push(new User(usr, pwd, eml));
+      return this.dbMockup[this.dbMockup.length - 1];
+    }
+  }
+}
