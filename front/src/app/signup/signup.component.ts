fa1cc45c06d476c02f975614be62babc81a0b5c0
Hamza El Meknassi <elmeknassi.h@gmail.com>
2018-12-20 15:14:52 +0100

    Added Reactive Forms and validation for Signup

--- front/src/app/signup/signup.component.ts
+++ front/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,20 +7,28 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./signup.component.sass']
 })
 export class SignupComponent implements OnInit {
-  @ViewChild('usrinput') private usrinput: ElementRef;
-  @ViewChild('pwdinput') private pwdinput: ElementRef;
-  @ViewChild('pw2input') private pw2input: ElementRef;
-  @ViewChild('emlinput') private emlinput: ElementRef;
+  private signupForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      password1: new FormControl('', [Validators.required]),
+      password2: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
   }
 
+  get username() { return this.signupForm.get('username'); }
+  get password1() { return this.signupForm.get('password1'); }
+  get password2() { return this.signupForm.get('password2'); }
+  get email() { return this.signupForm.get('email'); }
+
   register() {
-    console.log(this.usrinput.nativeElement.value,
-      this.pwdinput.nativeElement.value,
-      this.pw2input.nativeElement.value,
-      this.emlinput.nativeElement.value);
+    console.log(this.signupForm.value.username,
+      this.signupForm.value.password1,
+      this.signupForm.value.password2,
+      this.signupForm.value.email);
   }
 }
